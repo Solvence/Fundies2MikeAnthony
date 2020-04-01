ea695c6304896786feee3bc81aeffcedf368531c
Solvence <anthonyhmu@gmail.com>
2020-03-31 22:51:19 -0500

    removing seams started, little buggy

--- HW8/src/Squeeze.java
+++ HW8/src/Squeeze.java
@@ -6,96 +6,140 @@ import java.awt.Color;
 
 
 class SeamInfo {
-  
-  Pixel thisPixel;
+  APixel thisPixel;
   double totalWeight;
   SeamInfo cameFrom;
   
+  // the constructor
+  SeamInfo(APixel thisPixel, double totalWeight, SeamInfo cameFrom) {
+    this.thisPixel = thisPixel;
+    this.totalWeight = totalWeight;
+    this.cameFrom = cameFrom;
+  }
   
+  // constructor that only takes in two fields, used for the first row of pixels, when the seam has
+  // nothing it came from
+  SeamInfo(APixel thisPixel, double totalWeight) {
+    this.thisPixel = thisPixel;
+    this.totalWeight = totalWeight;
+    this.cameFrom = null;
+  }
 }
 
 class Picture extends World {
   APixel topLeft;
-  
-//  Picture(String imgFileName) {
-//    FromFileImage img = new FromFileImage("balloons.jpg");
-//    topLeft = new SentinelPixel();
-//    APixel prevRowPixel = topLeft;
-//    for (int row = 0; row < 343; row += 1) {
-//      APixel newPrevRowPixel = new SentinelPixel();
-//      newPrevRowPixel.up = prevRowPixel;
-//      newPrevRowPixel.down = prevRowPixel.down;
-//      newPrevRowPixel.up.down = newPrevRowPixel;
-//      newPrevRowPixel.down.up = newPrevRowPixel;
-//      prevRowPixel = newPrevRowPixel;
-//      APixel prevPixel = prevRowPixel;
-//      for (int col = 0; col < 800; col += 1) {
-//        Pixel thisPixel = new Pixel(prevPixel.up.right, new SentinelPixel(), prevPixel, 
-//            prevPixel.right, img.getColorAt(row, col));
-//        if (row == 0) {
-//          SentinelPixel newSent = new SentinelPixel();
-//          newSent.left = prevPixel.up;
-//          newSent.left = prevPixel.up.right;
-//          prevPixel.up.right = prevPixel.up;
-//          newSent.right.left = newSent;
-//          thisPixel.up = newSent;
-//          
-//          thisPixel.down = thisPixel.up;
-//        } else {
-//          thisPixel.up = prevPixel.up.right;
-//          thisPixel.down = thisPixel.up.down;
-//        }
-//        thisPixel.up.down = thisPixel;
-//        thisPixel.down.up = thisPixel;
-//        thisPixel.left.right = thisPixel;
-//        thisPixel.right.left = thisPixel;
-//        prevPixel = thisPixel;
-//      }
-//    }
-//  }
+  int width;
+  int height;
+  SeamInfo seamToRemove;  // the seam to remove in the current tick. If there is no seam to remove  
+  // currently, represents a default seam of the top left pixel, 0 weight, and null cameFrom
   
   Picture(String imgFileName) {
-    
-    ArrayList<SentinelPixel> rows = new ArrayList<SentinelPixel>();
-    ArrayList<SentinelPixel> cols = new ArrayList<SentinelPixel>();
-    
-    for (int i = 0; i < 344; i += 1) {
-      rows.add(new SentinelPixel());
-    }
-    
-    for (int i = 0; i < 800; i += 1) {
-      
-      cols.add(new SentinelPixel());
-      
-    }
-    
-    for (int i = 0; i < 344; i++) {
-      
-      for (int j = 0; i < 800; i++) {
-        
-        SentinelPixel rowSentinel = rows.get(i);
-        SentinelPixel colSentinel = cols.get(j);
-        
-        rowSentinel.addAtHead(Color.red);
-        colSentinel.addAtHead(Color.red);
-        
-        
+    FromFileImage img = new FromFileImage(imgFileName);
+    this.width = (int) img.getWidth();
+    this.height = (int) img.getHeight();
+    topLeft = new SentinelPixel();
+    APixel prevRowPixel = topLeft;
+    for (int row = 0; row < img.getHeight(); row += 1) {
+      APixel newPrevRowPixel = new SentinelPixel();
+      newPrevRowPixel.up = prevRowPixel;
+      newPrevRowPixel.down = prevRowPixel.down;
+      newPrevRowPixel.up.down = newPrevRowPixel;
+      newPrevRowPixel.down.up = newPrevRowPixel;
+      prevRowPixel = newPrevRowPixel;
+      APixel prevPixel = prevRowPixel;
+      for (int col = 0; col < img.getWidth(); col += 1) {
+        Pixel thisPixel = new Pixel(prevPixel.up.right, new SentinelPixel(), prevPixel, 
+            prevPixel.right, img.getColorAt(col, row));
+        if (row == 0) {
+          SentinelPixel newSent = new SentinelPixel();
+          newSent.left = prevPixel.up;
+          newSent.right = prevPixel.up.right;
+          prevPixel.up.right = newSent;
+          newSent.right.left = newSent;
+          thisPixel.up = newSent;
+          thisPixel.down = newSent;
+        } else {
+          thisPixel.up = prevPixel.up.right;
+          thisPixel.down = thisPixel.up.down;
+        }
+        thisPixel.up.down = thisPixel;
+        thisPixel.down.up = thisPixel;
+        thisPixel.left.right = thisPixel;
+        thisPixel.right.left = thisPixel;
+        prevPixel = thisPixel;
       }
-      
     }
-    
-    FromFileImage img = new FromFileImage("balloons.jpg");
-    
-    
   }
   
   public WorldScene makeScene() {
     WorldImage img = new FromFileImage("balloons.jpg");
+    ComputedPixelImage cpi = new ComputedPixelImage((int) img.getWidth(), (int) img.getHeight());
+    APixel nextRowPixel = topLeft;
+    for (int row = 0; row < img.getHeight(); row += 1) {
+      nextRowPixel = nextRowPixel.down;
+      APixel nextPixel = nextRowPixel.right;
+      for (int col = 0; col < img.getWidth(); col += 1) {
+        cpi.setPixel(col, row, nextPixel.color);
+        nextPixel = nextPixel.right;
+      }
+    }
     
     WorldScene ws = new WorldScene(800, 344);
-    ws.placeImageXY(img, 400, 172);
+    ws.placeImageXY(cpi, 400, 172);
     
     return ws;
+  }
+  
+  public void onTick() {
+    if (this.seamToRemove != null && this.seamToRemove.cameFrom != null) {
+      APixel currentPixel = this.seamToRemove.thisPixel;
+      currentPixel.down.remove();
+      while (this.seamToRemove.cameFrom != null) {
+        currentPixel = this.seamToRemove.thisPixel;
+        currentPixel.remove();
+        this.seamToRemove = this.seamToRemove.cameFrom;
+      }
+    } else {
+      ArrayList<ArrayList<SeamInfo>> seams = new ArrayList<ArrayList<SeamInfo>>();
+      
+      APixel nextRowPixel = topLeft;
+      for (int row = 0; row < this.height; row += 1) {
+        nextRowPixel = nextRowPixel.down;
+        APixel nextPixel = nextRowPixel.right;
+        seams.add(new ArrayList<SeamInfo>());
+        for (int col = 0; col < this.width; col += 1) {
+          if (row == 0) {
+            seams.get(row).add(new SeamInfo(nextPixel, nextPixel.calculateEnergy()));
+          } else {
+            SeamInfo cameFrom = seams.get(row - 1).get(col);
+            if (col != 0 && seams.get(row - 1).get(col - 1).totalWeight < cameFrom.totalWeight) {
+              cameFrom = seams.get(row - 1).get(col - 1);
+            }
+            if (col != width - 1 
+                && seams.get(row - 1).get(col + 1).totalWeight < cameFrom.totalWeight) {
+              cameFrom = seams.get(row - 1).get(col + 1);
+            }
+            
+            seams.get(row).add(new SeamInfo(nextPixel, nextPixel.calculateEnergy(), cameFrom));
+          }
+          nextPixel = nextPixel.right;
+        }
+      }
+      SeamInfo seamToHighlight = seams.get(height - 1).get(0);
+      
+      for (int col = 1; col < this.width; col += 1) {
+        if (seams.get(height - 1).get(col).totalWeight < seamToHighlight.totalWeight) {
+          seamToHighlight = seams.get(height - 1).get(col);
+        }
+      }
+      
+      this.seamToRemove = seamToHighlight;
+      
+      while (seamToHighlight.cameFrom != null) {
+        seamToHighlight.thisPixel.highlight();
+        seamToHighlight = seamToHighlight.cameFrom;
+      }
+    }
   }
 }
 
@@ -126,20 +170,39 @@ abstract class APixel {
     return ((double) (color.getRed() + color.getGreen() + color.getBlue())) / 3 / 255.0;
   }
   
-  void setUp(APixel newUp) {
-    this.up = newUp;
+  double calculateHorizEnergy() {
+    
+    return (this.up.left.calculateBrightness() +
+        2 * this.left.calculateBrightness() +
+        this.down.left.calculateBrightness()) - (
+        this.up.right.calculateBrightness() +
+        2 * this.right.calculateBrightness() +
+        this.down.right.calculateBrightness());   
   }
   
-  void setDown(APixel newDown) {
-    this.down = newDown;
+  double calculateVertEnergy() {
+    return (this.up.left.calculateBrightness() +
+        2 * this.up.calculateBrightness() +
+        this.up.right.calculateBrightness()) - (
+        this.down.left.calculateBrightness() +
+        2 * this.down.calculateBrightness() +
+        this.down.right.calculateBrightness());
   }
   
-  void setLeft(APixel newLeft) {
-    this.left = newLeft;
+  double calculateEnergy() {
+    return Math.sqrt(Math.pow(this.calculateHorizEnergy(), 2) +
+        Math.pow(this.calculateVertEnergy(), 2));
   }
   
-  void setRight(APixel newRight) {
-    this.right = newRight;
+  void remove() {
+    this.up.down = this.down;
+    this.down.up = this.up;
+    this.left.right = this.right;
+    this.right.left = this.left;
+  }
+  
+  void highlight() {
+    this.color = Color.RED;
   }
 }
 
@@ -157,60 +220,18 @@ class SentinelPixel extends APixel {
     
     this.color = Color.BLACK;
   }
-  
-  void addAtHead(Color color) {
-    new Pixel(this.up, this.down, this.left, this.right, color);
-  }
 }
 
 class Pixel extends APixel {
   
-//  Pixel(APixel up, APixel down, APixel left, APixel right, Color color) {
-//    super(up, down, left, right, color);
-//  }
-  
   Pixel(APixel up, APixel down, APixel left, APixel right, Color color) {
     super(up, down, left, right, color);
-    
-    this.up.setDown(this);
-    this.down.setUp(this);
-    this.left.setRight(this);
-    this.right.setLeft(this);
   }
   
   
-  
-  double calculateHorizEnergy() {
-    
-    return (this.up.left.calculateBrightness() +
-        2 * this.left.calculateBrightness() +
-        this.down.left.calculateBrightness()) - (
-        this.up.right.calculateBrightness() +
-        2 * this.right.calculateBrightness() +
-        this.down.right.calculateBrightness());
-        
-        
-  }
-  
-  double calculateVertEnergy() {
-    
-    return (this.up.left.calculateBrightness() +
-        2 * this.up.calculateBrightness() +
-        this.up.right.calculateBrightness()) - (
-        this.down.left.calculateBrightness() +
-        2 * this.down.calculateBrightness() +
-        this.down.right.calculateBrightness());
-        
-  }
-  
-  double calculateEnergy() {
-    return Math.sqrt(Math.pow(this.calculateHorizEnergy(), 2) +
-        Math.pow(this.calculateVertEnergy(), 2));
-  }
   
   
 }
-
 
 class ExamplesSqueeze {
   
@@ -219,7 +240,7 @@ class ExamplesSqueeze {
   void testWorld (Tester t) {
     
     Picture p = new Picture("balloons.jpg");
-    p.bigBang(800, 344);
+    p.bigBang(800, 344, 1.0 / 4);
         
     
   }
