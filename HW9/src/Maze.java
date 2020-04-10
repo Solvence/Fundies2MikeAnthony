10337d8c84b232b85b1f6b7046b8f4c737628aca
Solvence <anthonyhmu@gmail.com>
2020-04-10 17:36:18 -0500

    drawing maze complete

--- HW9/src/Maze.java
+++ HW9/src/Maze.java
@@ -13,10 +13,14 @@ class Maze extends World {
   private final int width;
   private final int height;
   private final ArrayList<Edge> edgesInMaze;
+  private final WorldScene mazeScene;
+  private final int cellSize; // in pixels
   
   Maze(int width, int height, Random r) {
     this.width = width;
     this.height = height;
+    
+    this.cellSize = Math.min(1250 / this.width, 750 / this.height);
     
     ArrayList<ArrayList<Posn>> mazeLocations = new ArrayList<ArrayList<Posn>>();
     for (int row = 0; row < height; row += 1) {
@@ -27,10 +31,53 @@ class Maze extends World {
     }
     
     this.edgesInMaze = new SpanningTree(mazeLocations, r).getSpanningTree();
+    
+    WorldScene mazeSceneInProgress = new WorldScene(this.width * this.cellSize, 
+        this.height * this.cellSize);
+    mazeSceneInProgress.placeImageXY(new RectangleImage(this.width * this.cellSize, 
+        this.height * this.cellSize, 
+        OutlineMode.OUTLINE, Color.BLACK), this.width * this.cellSize / 2, 
+        this.height * this.cellSize / 2);
+        
+    WorldImage WALL = new LineImage(new Posn(0, this.cellSize), Color.BLACK);
+    WorldImage WALL_H = new RotateImage(WALL, 90);
+    for (int row = 0; row < this.height; row += 1) {
+      for (int col = 0; col < this.width; col += 1) {
+        
+        boolean hasRightWall = true;
+        boolean hasDownWall = true;
+        for (Edge e : this.edgesInMaze) {
+          if (e.src.equals(new Posn(col, row)) && e.dest.equals(new Posn(col + 1, row)) || 
+              e.dest.equals(new Posn(col, row)) && e.src.equals(new Posn(col + 1, row))) {
+            hasRightWall = false;
+          }
+          if (e.src.equals(new Posn(col, row)) && e.dest.equals(new Posn(col, row + 1)) || 
+              e.dest.equals(new Posn(col, row)) && e.src.equals(new Posn(col, row + 1))) {
+            hasDownWall = false;
+          }
+        }
+        
+        if (hasRightWall) {
+          mazeSceneInProgress.placeImageXY(WALL, (col + 1) * this.cellSize, 
+              row * this.cellSize + this.cellSize / 2);
+        }
+        
+        if (hasDownWall) {
+          mazeSceneInProgress.placeImageXY(WALL_H, col * this.cellSize + this.cellSize / 2, 
+              (row + 1) * this.cellSize);
+        }
+      }
+    }
+    
+    this.mazeScene = mazeSceneInProgress;
   }
   
   Maze(int width, int height) {
     this(width, height, new Random());
+  }
+  
+  public WorldScene makeScene() {
+    return this.mazeScene;
   }
 }
 
@@ -65,7 +112,7 @@ class SpanningTree {
         }
       }
     }
-    this.edgesInTree.sort(new EdgeComparator());
+    this.worklist.sort(new EdgeComparator());
   }
   
   SpanningTree(ArrayList<ArrayList<Posn>> graphLocations) {
@@ -84,7 +131,7 @@ class SpanningTree {
         this.worklist.remove(0);
       }
     }
-    return edgesInTree;
+    return this.edgesInTree;
   }
   
   boolean isNotSpanning() {
@@ -108,13 +155,12 @@ class SpanningTree {
   }
   
   void union(Posn src, Posn dest) {
-    representatives.replace(src, dest);
+    representatives.replace(this.find(src), this.find(dest));
   }
   
   void sortEdges() {
     this.edgesInTree.sort(new EdgeComparator());
   }
-  
 }
 
 class Edge implements Comparable<Edge> {
@@ -139,6 +185,13 @@ class Edge implements Comparable<Edge> {
 class EdgeComparator implements Comparator<Edge> {
   public int compare(Edge o1, Edge o2) {
     return o1.compareTo(o2);
+  }
+}
+
+class ExamplesMaze {
+  void testWorld(Tester t) {
+    
+    new Maze(100, 60).bigBang(1250, 750, 1.0 / 16);
   }
 }
 
