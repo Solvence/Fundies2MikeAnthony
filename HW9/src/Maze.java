863b5ee3e014a6b10188ae7253d21ab7860715d2
Solvence <anthonyhmu@gmail.com>
2020-04-13 21:01:21 -0500

    many tests done

--- HW9/src/Maze.java
+++ HW9/src/Maze.java
@@ -24,6 +24,7 @@ class Maze extends World {
   private final ArrayList<Posn> squaresColored;
   private final HashMap<Posn, Posn> playerPath;
   private SpanningTree mazeTree;
+  private final Random r;
   
   Maze(int width, int height, Random r) {
     this.width = width;
@@ -36,6 +37,7 @@ class Maze extends World {
     this.squaresColored = new ArrayList<Posn>();
     this.squaresColored.add(new Posn(0, 0));
     this.playerPath = new HashMap<Posn, Posn>();
+    this.r = r;
     
     this.cellSize = Math.min(1250 / this.width, 750 / this.height);
 
@@ -46,7 +48,7 @@ class Maze extends World {
         mazeLocations.get(row).add(new Posn(col, row));
       }
     }
-    this.mazeTree =  new SpanningTree(mazeLocations, new Random(), this.height * this.width);
+    this.mazeTree =  new SpanningTree(mazeLocations, this.r);
     this.edgesInMaze = this.mazeTree.getSpanningTree();
     
     this.mazeScene = this.mazeTree.getMazeScene(this.cellSize);
@@ -144,7 +146,7 @@ class Maze extends World {
         }
       }
       
-      this.mazeTree = new SpanningTree(mazeLocations, new Random(), this.height * this.width);
+      this.mazeTree = new SpanningTree(mazeLocations, this.r);
       this.edgesInMaze = this.mazeTree.getSpanningTree();
       
       this.mazeScene = this.mazeTree.getMazeScene(this.cellSize);
@@ -221,9 +223,14 @@ class Maze extends World {
     }
   }
   
+  // computes the path from the top left corner to the bottom right corner of the maze
+  // EFFECT: modifies squaresToColor to contain all the positions that the searching algorithm went
+  // through before finding the solution. Modifies the finalPath field to contain all the positions
+  // in the solution path
   ArrayList<Posn> searchMaze(boolean BFS) {
     
-    ArrayDeque<Posn> worklist = new ArrayDeque<Posn>(); // A Queue or a Stack, depending on the algorithm
+    ArrayDeque<Posn> worklist = new ArrayDeque<Posn>(); // A Queue or a Stack, depending on the 
+    // algorithm
     worklist.add(new Posn(0,0));
     
     HashMap<Posn, Boolean> visitedCells = new HashMap<Posn, Boolean>();
@@ -236,10 +243,10 @@ class Maze extends World {
       worklist.removeFirst();
       
       if (nextItem.equals(new Posn(this.width - 1, this.height - 1))){
-        squaresToColor.add(nextItem);
+        this.squaresToColor.add(nextItem);
         return this.reconstruct(cameFromPosn, nextItem);
       } else if (visitedCells.get(nextItem) == null) {
-        squaresToColor.add(nextItem);
+        this.squaresToColor.add(nextItem);
         visitedCells.put(nextItem, true);
         ArrayList<Posn> neighbors = this.mazeMap.get(nextItem);
         for (Posn neighbor : neighbors) {
@@ -261,7 +268,6 @@ class Maze extends World {
     ArrayList<Posn> steps = new ArrayList<Posn>();
     Posn currPosition = new Posn(nextItem.x, nextItem.y);
     while(!currPosition.equals(new Posn(0,0))) {
-      System.out.println(currPosition.x + ", " + currPosition.y);
       steps.add(new Posn(currPosition.x, currPosition.y));
       this.finalPath.add(new Posn(currPosition.x, currPosition.y));
       currPosition = cameFromPosn.get(currPosition);
@@ -280,10 +286,15 @@ class SpanningTree {
   private final ArrayList<ArrayList<Posn>> graphLocations;
   private final int nodeCount;
   
-  SpanningTree(ArrayList<ArrayList<Posn>> graphLocations, Random r, int nodeCount) {
+  SpanningTree(ArrayList<ArrayList<Posn>> graphLocations, Random r) {
     this.representatives = new HashMap<Posn, Posn>();
     this.graphLocations = graphLocations;
-    this.nodeCount = nodeCount;
+    int nodes = 0;
+    for (ArrayList<Posn> arr : graphLocations) {
+      nodes += arr.size();
+    }
+    this.nodeCount = nodes;
+    
     for (ArrayList<Posn> arr : graphLocations) {
       for (Posn p : arr) {
         this.representatives.put(p, p);
@@ -306,13 +317,17 @@ class SpanningTree {
       }
     }
     this.worklist.sort(new EdgeComparator());
-    System.out.print("Edges Sorted"); 
   }
   
-  SpanningTree(ArrayList<ArrayList<Posn>> graphLocations, int nodeCount) {
-    this(graphLocations, new Random(), nodeCount);
+  SpanningTree(ArrayList<ArrayList<Posn>> graphLocations) {
+    this(graphLocations, new Random());
   }
   
+  // Computes the minimum spanning tree using Kruskal's algorithm on the worklist of weighted edges
+  // EFFECT: removes enough nodes from the worklist to achieve a spanning tree, modifies the
+  // edgesInTree to contain all the edges in the resulting spanning tree, and modifies the
+  // representatives hashmap to represent a spanning tree, where every node's root representative is
+  // the same
   ArrayList<Edge> getSpanningTree() {
     int counter = 0;
     while (counter < this.nodeCount - 1) {
@@ -329,7 +344,10 @@ class SpanningTree {
     return this.edgesInTree;
   }
   
-  Posn find(Posn key) {
+  // Computes the root of the tree with the given Posn in it
+  // this method is private as users of the SpanningTree class should not be able to call this
+  // method directly as it only deals with private fields in this class
+  private Posn find(Posn key) {
     if (representatives.get(key).equals(key)) {
       return key;
     } else {
@@ -337,7 +355,12 @@ class SpanningTree {
     }
   }
   
-  void union(Posn src, Posn dest) {
+  // Combines the trees with the two given Posns into one tree
+  // this method is private as users of the SpanningTree class should not be able to call this
+  // method directly as it only deals with private fields in this class
+  // EFFECT: modifies the representatives hashmap to account for the new connection: the first given
+  // Posn's root's representative will be the second given Posn's root
+  private void union(Posn src, Posn dest) {
     representatives.replace(this.find(src), this.find(dest));
   }
   
@@ -360,11 +383,13 @@ class SpanningTree {
         boolean hasDownWall = true;
         for (Edge e : this.edgesInTree) {
           if (e.src.equals(new Posn(col, row)) && e.dest.equals(new Posn(col + 1, row)) || 
-              e.dest.equals(new Posn(col, row)) && e.src.equals(new Posn(col + 1, row))) {
+              e.dest.equals(new Posn(col, row)) && e.src.equals(new Posn(col + 1, row)) ||
+              col == width - 1) {
             hasRightWall = false;
           }
           if (e.src.equals(new Posn(col, row)) && e.dest.equals(new Posn(col, row + 1)) || 
-              e.dest.equals(new Posn(col, row)) && e.src.equals(new Posn(col, row + 1))) {
+              e.dest.equals(new Posn(col, row)) && e.src.equals(new Posn(col, row + 1)) ||
+              row == height - 1) {
             hasDownWall = false;
           }
         }
@@ -447,6 +472,234 @@ class ExamplesMaze {
    *  The User is Notified of the completion of the Game
    *  The Solution Path is Displayed. 
    */
+  
+  Maze maze1;
+  Maze maze2;
+  
+  SpanningTree tree1;
+  SpanningTree tree2;
+  
+  void initTestConditions() {
+    maze1 = new Maze(4, 4, new Random(360));
+    maze2 = new Maze(3, 3, new Random(350));
+    
+    ArrayList<ArrayList<Posn>> mazeLocations = new ArrayList<ArrayList<Posn>>();
+    for (int row = 0; row < 4; row += 1) {
+      mazeLocations.add(new ArrayList<Posn>());
+      for (int col = 0; col < 4; col += 1) {
+        mazeLocations.get(row).add(new Posn(col, row));
+      }
+    }
+    tree1 = new SpanningTree(mazeLocations, new Random(360));
+    
+    ArrayList<ArrayList<Posn>> mazeLocations2 = new ArrayList<ArrayList<Posn>>();
+    for (int row = 0; row < 3; row += 1) {
+      mazeLocations2.add(new ArrayList<Posn>());
+      for (int col = 0; col < 3; col += 1) {
+        mazeLocations2.get(row).add(new Posn(col, row));
+      }
+    }
+    tree2 = new SpanningTree(mazeLocations2, new Random(350));
+  }
+  
+  // test that the getSpanningTree method in SpanningTree functions properly
+  void testGetSpanningTree(Tester t) {
+    this.initTestConditions();
+    
+    ArrayList<Edge> tree1Edges = new ArrayList<Edge>();
+    tree1Edges.add(new Edge(new Posn(1, 3), new Posn(2, 3), 10));
+    tree1Edges.add(new Edge(new Posn(2, 1), new Posn(2, 2), 27));
+    tree1Edges.add(new Edge(new Posn(2, 0), new Posn(2, 1), 48));
+    tree1Edges.add(new Edge(new Posn(3, 2), new Posn(3, 3), 61));
+    tree1Edges.add(new Edge(new Posn(0, 1), new Posn(0, 2), 76));
+    tree1Edges.add(new Edge(new Posn(2, 3), new Posn(3, 3), 81));
+    tree1Edges.add(new Edge(new Posn(2, 0), new Posn(3, 0), 107));
+    tree1Edges.add(new Edge(new Posn(2, 1), new Posn(3, 1), 260));
+    tree1Edges.add(new Edge(new Posn(0, 0), new Posn(0, 1), 276));
+    tree1Edges.add(new Edge(new Posn(1, 2), new Posn(1, 3), 299));
+    tree1Edges.add(new Edge(new Posn(2, 2), new Posn(3, 2), 357));
+    tree1Edges.add(new Edge(new Posn(1, 0), new Posn(2, 0), 403));
+    tree1Edges.add(new Edge(new Posn(1, 0), new Posn(1, 1), 447));
+    tree1Edges.add(new Edge(new Posn(0, 1), new Posn(1, 1), 517));
+    tree1Edges.add(new Edge(new Posn(0, 2), new Posn(0, 3), 629));
+    t.checkExpect(tree1.getSpanningTree(), tree1Edges);
+    
+    ArrayList<Edge> tree2Edges = new ArrayList<Edge>();
+    tree2Edges.add(new Edge(new Posn(2, 0), new Posn(2, 1), 88));
+    tree2Edges.add(new Edge(new Posn(1, 2), new Posn(2, 2), 190));
+    tree2Edges.add(new Edge(new Posn(0, 0), new Posn(1, 0), 272));
+    tree2Edges.add(new Edge(new Posn(1, 1), new Posn(1, 2), 398));
+    tree2Edges.add(new Edge(new Posn(0, 0), new Posn(0, 1), 501));
+    tree2Edges.add(new Edge(new Posn(0, 1), new Posn(0, 2), 529));
+    tree2Edges.add(new Edge(new Posn(1, 1), new Posn(2, 1), 582));
+    tree2Edges.add(new Edge(new Posn(0, 2), new Posn(1, 2), 618));
+    t.checkExpect(tree2.getSpanningTree(), tree2Edges);
+  }
+  
+  // test that the getMazeScene method in SpanningTree functions properly
+  void testGetMazeScene(Tester t) {
+    this.initTestConditions();
+    WorldScene maze0Scene = new WorldScene(80, 80);
+    maze0Scene.placeImageXY(new RectangleImage(80, 80, OutlineMode.OUTLINE, Color.BLACK), 40, 40);
+       
+    WorldImage WALL = new LineImage(new Posn(0, 20), Color.BLACK);
+    WorldImage WALL_H = new RotateImage(WALL, 90);
+    for (int row = 0; row < 4; row += 1) {
+      for (int col = 0; col < 4; col += 1) {
+        maze0Scene.placeImageXY(WALL, (col + 1) * 20, row * 20 + 10);
+        maze0Scene.placeImageXY(WALL_H, col * 20 + 10, (row + 1) * 20);
+      }
+    }
+   
+    t.checkExpect(tree1.getMazeScene(20), maze0Scene);
+   
+    tree1.getSpanningTree();
+    tree2.getSpanningTree();
+    // mazeScene would practically only be called after getSpanningTree(), otherwise it would just
+    // return a checkerboard, as tested above
+   
+    WorldScene maze1Scene = new WorldScene(80, 80);
+    maze1Scene.placeImageXY(new RectangleImage(80, 80, OutlineMode.OUTLINE, Color.BLACK), 40, 40);
+   
+    for (int row = 0; row < 4; row += 1) {
+      for (int col = 0; col < 4; col += 1) {
+        if ((col == 0 && row != 1) || (col == 1 && row == 1) || (col == 1 && row == 2)) {
+          maze1Scene.placeImageXY(WALL, (col + 1) * 20, row * 20 + 10);
+        }
+        if ((col == 1 && row == 1) || (col == 2 && row == 2) || (col == 3 && row < 2)) {
+          maze1Scene.placeImageXY(WALL_H, col * 20 + 10, (row + 1) * 20);
+        }
+      }
+    }
+   
+    t.checkExpect(tree1.getMazeScene(20), maze1Scene);
+   
+    WorldScene maze2Scene = new WorldScene(60, 60);
+    maze2Scene.placeImageXY(new RectangleImage(60, 60, OutlineMode.OUTLINE, Color.BLACK), 30, 30);
+   
+    for (int row = 0; row < 3; row += 1) {
+      for (int col = 0; col < 3; col += 1) {
+        if ((col == 0 && row == 1) || (col == 1 && row == 0)) {
+          maze2Scene.placeImageXY(WALL, (col + 1) * 20, row * 20 + 10);
+        }
+        if ((col == 1 && row == 0) || (col == 2 && row == 1)) {
+          maze2Scene.placeImageXY(WALL_H, col * 20 + 10, (row + 1) * 20);
+        }
+      }
+    }
+   
+    t.checkExpect(tree2.getMazeScene(20), maze2Scene);
+  }
+ 
+  // tests that the searchMaze method in the Maze class functions properly
+  void testSearchMaze(Tester t) {
+    this.initTestConditions();
+    
+    ArrayList<Posn> maze1Path = new ArrayList<Posn>();
+    maze1Path.add(new Posn(3, 3));
+    maze1Path.add(new Posn(3, 2));
+    maze1Path.add(new Posn(2, 2));
+    maze1Path.add(new Posn(2, 1));
+    maze1Path.add(new Posn(2, 0));
+    maze1Path.add(new Posn(1, 0));
+    maze1Path.add(new Posn(1, 1));
+    maze1Path.add(new Posn(0, 1));
+    maze1Path.add(new Posn(0, 0));
+    
+    t.checkExpect(maze1.searchMaze(true), maze1Path);
+    t.checkExpect(maze1.searchMaze(false), maze1Path);
+    
+    ArrayList<Posn> maze2Path = new ArrayList<Posn>();
+    maze2Path.add(new Posn(2, 2));
+    maze2Path.add(new Posn(1, 2));
+    maze2Path.add(new Posn(0, 2));
+    maze2Path.add(new Posn(0, 1));
+    maze2Path.add(new Posn(0, 0));
+    
+    t.checkExpect(maze2.searchMaze(true), maze2Path);
+    t.checkExpect(maze2.searchMaze(false), maze2Path);
+    
+    maze2.onKeyEvent("n");
+    
+    maze2Path = new ArrayList<Posn>();
+    maze2Path.add(new Posn(2, 2));
+    maze2Path.add(new Posn(2, 1));
+    maze2Path.add(new Posn(2, 0));
+    maze2Path.add(new Posn(1, 0));
+    maze2Path.add(new Posn(0, 0));
+    
+    t.checkExpect(maze2.searchMaze(true), maze2Path);
+    t.checkExpect(maze2.searchMaze(false), maze2Path);
+  }
+  
+  // tests that the reconstruct method in the Maze class functions properly
+  void testReconstruct(Tester t) {
+    this.initTestConditions();
+    
+    HashMap<Posn, Posn> diagonalMap = new HashMap<Posn, Posn>();
+    diagonalMap.put(new Posn(1, 0), new Posn(0, 0));
+    diagonalMap.put(new Posn(1, 1), new Posn(1, 0));
+    diagonalMap.put(new Posn(2, 1), new Posn(1, 1));
+    diagonalMap.put(new Posn(2, 2), new Posn(2, 1));
+    diagonalMap.put(new Posn(3, 2), new Posn(2, 2));
+    diagonalMap.put(new Posn(3, 3), new Posn(3, 2));
+    
+    HashMap<Posn, Posn> bottomMap3 = new HashMap<Posn, Posn>();
+    bottomMap3.put(new Posn(0, 1), new Posn(0, 0));
+    bottomMap3.put(new Posn(0, 2), new Posn(0, 1));
+    bottomMap3.put(new Posn(1, 2), new Posn(0, 2));
+    bottomMap3.put(new Posn(2, 2), new Posn(1, 2));
+    
+    HashMap<Posn, Posn> bottomMap4 = new HashMap<Posn, Posn>();
+    bottomMap4.put(new Posn(0, 1), new Posn(0, 0));
+    bottomMap4.put(new Posn(0, 2), new Posn(0, 1));
+    bottomMap4.put(new Posn(0, 3), new Posn(0, 2));
+    bottomMap4.put(new Posn(1, 3), new Posn(0, 3));
+    bottomMap4.put(new Posn(2, 3), new Posn(1, 3));
+    bottomMap4.put(new Posn(3, 3), new Posn(2, 3));
+    
+    ArrayList<Posn> maze1DiagonalPath = new ArrayList<Posn>();
+    maze1DiagonalPath.add(new Posn(3, 3));
+    maze1DiagonalPath.add(new Posn(3, 2));
+    maze1DiagonalPath.add(new Posn(2, 2));
+    maze1DiagonalPath.add(new Posn(2, 1));
+    maze1DiagonalPath.add(new Posn(1, 1));
+    maze1DiagonalPath.add(new Posn(1, 0));
+    maze1DiagonalPath.add(new Posn(0, 0));
+    
+    ArrayList<Posn> maze1BottomPath = new ArrayList<Posn>();
+    maze1BottomPath.add(new Posn(3, 3));
+    maze1BottomPath.add(new Posn(2, 3));
+    maze1BottomPath.add(new Posn(1, 3));
+    maze1BottomPath.add(new Posn(0, 3));
+    maze1BottomPath.add(new Posn(0, 2));
+    maze1BottomPath.add(new Posn(0, 1));
+    maze1BottomPath.add(new Posn(0, 0));
+    
+    ArrayList<Posn> maze2DiagonalPath = new ArrayList<Posn>();
+    maze2DiagonalPath.add(new Posn(2, 2));
+    maze2DiagonalPath.add(new Posn(2, 1));
+    maze2DiagonalPath.add(new Posn(1, 1));
+    maze2DiagonalPath.add(new Posn(1, 0));
+    maze2DiagonalPath.add(new Posn(0, 0));
+    
+    ArrayList<Posn> maze2BottomPath = new ArrayList<Posn>();
+    maze2BottomPath.add(new Posn(2, 2));
+    maze2BottomPath.add(new Posn(1, 2));
+    maze2BottomPath.add(new Posn(0, 2));
+    maze2BottomPath.add(new Posn(0, 1));
+    maze2BottomPath.add(new Posn(0, 0));
+    
+    t.checkExpect(maze1.reconstruct(diagonalMap, new Posn(3, 3)), maze1DiagonalPath);
+    t.checkExpect(maze1.reconstruct(bottomMap4, new Posn(3, 3)), maze1BottomPath);
+    t.checkExpect(maze2.reconstruct(diagonalMap, new Posn(2, 2)), maze2DiagonalPath);
+    t.checkExpect(maze2.reconstruct(bottomMap3, new Posn(2, 2)), maze2BottomPath);
+  }
+  
+  // test the compareTo method in the Edge class
+  /*void testCompareTo(Tester t) {
+    Edge e1 = 
+  }*/
   
   void testWorld(Tester t) {
     new Maze(30, 30).bigBang(1250, 750, 1.0 / 100);
